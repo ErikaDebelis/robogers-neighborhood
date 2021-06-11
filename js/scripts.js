4f9ad0be4cbd4af39e1574343f8a32559d302ec8
Erika Debelis <Erika.Debelis@gmail.com>
2021-06-11 11:46:03 -0700

    WIP: start UI logic + debugging business

--- js/scripts.js
+++ js/scripts.js
@@ -8,9 +8,6 @@
 let robogersOutput = []; //needed- must build dresser before putting away clothes
 
 let inputForm = $("#robogersInteger").val(); //parseInt not needed currently- chose to turn all integers to strings in order to condense logic needed once dealing with beep boops. 
-const ruleThree = [];
-const ruleTwo = [];
-const ruleOne = [];
 
 const inputArrayReturned = [];
 
@@ -20,25 +17,27 @@ function robogersSequence(number) {
     inputArrayReturned.push(i);
   }
 }
+let inputToString = inputArrayReturned.toString(); //changes for loop sequence from numbers to a string
+let inputToSeparateStrings = inputToString.split(",")
 
 //input = 6
 // inputArrayReturned = [6, 5, 4, 3, 2, 1, 0] 
 //forEach 
-inputArrayReturned.forEach(function(element) {
+inputToSeparateStrings.forEach(function(element) {
   if (inputForm.includes("3")){
-  let ruleThree = standardList.replace("3", "Won't you be my neighbor?");
+  let ruleThree = inputForm.replace("3", "Won't you be my neighbor?");
   robogersOutput.push(ruleThree); //need to use push not concat so rules stack
     if (inputForm.includes("2")){
-      let ruleTwo = standardList.replace("2", "Boop!");
+      let ruleTwo = inputForm.replace("2", "Boop!");
       robogersOutput.push(ruleTwo); // nested in rule three so numbers like 23 will have already been changed to 'will you be my neighbor'
       if (inputForm.includes("1")){
-        let ruleThree = standardList.replace("1", "Beep!");
+        let ruleOne = inputForm.replace("1", "Beep!");
         robogersOutput.push(ruleOne); //nested in rule two so numbers like 12 will have already been changed to 'boop!'
       }
     }
-}else{
-  robogersOutput = standardList(parsInt(inputForm));
-}
+  }else{
+    return inputArrayReturned;
+  }
 });
 
 
@@ -47,3 +46,10 @@ inputArrayReturned.forEach(function(element) {
 
 //UI Logic
 $(document).ready(function() {
+  ("#robogers-form").submit(function() {
+    event.preventDefault(); //nested?
+    const inputSubmitted = $("input#robogersInteger").val();
+    const robogersResult = robogersSequence(inputSubmitted);
+    $("#result").text(robogersResult);
+  });
+});
