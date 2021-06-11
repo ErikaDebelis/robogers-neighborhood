1dc2c1d13bbc34c1ac944c816c0d66f5fa19ece4
Erika Debelis <Erika.Debelis@gmail.com>
2021-06-11 11:06:47 -0700

    WIP: create for loop & for each loop

--- js/scripts.js
+++ js/scripts.js
@@ -4,44 +4,42 @@
 
 
 //Business Logic
-const robogersCompleteOutput = ["0", "Beep!", "Boop!", "Won't you be my neighbor?", "4", "5", "6", "7", "8", "9", "10", "11", "12", "Won't you be my neighbor?", "14", "15", "16", "17", "18", "19", "20", "Boop", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "Won't you be my neighbor?"]
 
 let robogersOutput = []; //needed- must build dresser before putting away clothes
 
 let inputForm = $("#robogersInteger").val(); //parseInt not needed currently- chose to turn all integers to strings in order to condense logic needed once dealing with beep boops. 
+const ruleThree = [];
+const ruleTwo = [];
+const ruleOne = [];
+
+const inputArrayReturned = [];
 
 
-//theres gotta be a more effcient way- range?
-function robogersSequence(robogersCompleteOutput) {
-  for (let i = 0; i < robogersCompleteOutput.length; i++){
-    if (inputForm === robogersCompleteOutput[0]) {
-      robogersOutput = robogersCompleteOutput.slice(0)
-      return (robogersOutput);
-    } else if (inputForm === "1") {
-      robogersOutput = robogersCompleteOutput.slice(1)
-      return (robogersOutput);
-    }else if (inputForm === "2") {
-      robogersOutput = robogersCompleteOutput.slice(2)
-      return (robogersOutput);
-    }else if (inputForm === "3") {
-      robogersOutput = robogersCompleteOutput.slice(3)
-      return (robogersOutput);
-    }else if (inputForm === "4-12" || inputForm === "14-20" || "22 - 31") { // unsure of formatting
-      robogersOutput = robogersCompleteOutput.slice(parsInt(inputForm);
-      return (robogersOutput);
-    } else if (inputForm === "13") {
-      robogersOutput = robogersCompleteOutput.slice(13)
-      return (robogersOutput);
-    } else if (inputForm === "21") {
-      robogersOutput = robogersCompleteOutput.slice(21)
-      return (robogersOutput);
-    } else if (inputForm === "32") {
-      return (robogersOutput);
-    } else{
-      return (#error);
-    }
+function robogersSequence(number) {
+  for (let i = number; i >= 0; i--){ //for loop will start with inputted number and loop till it hit 0
+    inputArrayReturned.push(i);
   }
 }
+
+//input = 6
+// inputArrayReturned = [6, 5, 4, 3, 2, 1, 0] 
+//forEach 
+inputArrayReturned.forEach(function(element) {
+  if (inputForm.includes("3")){
+  let ruleThree = standardList.replace("3", "Won't you be my neighbor?");
+  robogersOutput.push(ruleThree); //need to use push not concat so rules stack
+    if (inputForm.includes("2")){
+      let ruleTwo = standardList.replace("2", "Boop!");
+      robogersOutput.push(ruleTwo); // nested in rule three so numbers like 23 will have already been changed to 'will you be my neighbor'
+      if (inputForm.includes("1")){
+        let ruleThree = standardList.replace("1", "Beep!");
+        robogersOutput.push(ruleOne); //nested in rule two so numbers like 12 will have already been changed to 'boop!'
+      }
+    }
+}else{
+  robogersOutput = standardList(parsInt(inputForm));
+}
+});
 
 
 
