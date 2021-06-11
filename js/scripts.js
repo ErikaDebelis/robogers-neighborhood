ee33c6d918bcc49c508bf5574e9e2fed1177c5d3
Erika Debelis <Erika.Debelis@gmail.com>
2021-06-11 16:54:04 -0700

    WIP: could not debug

--- js/scripts.js
+++ js/scripts.js
@@ -9,53 +9,51 @@ let userInput = $("#robogersInteger").val();
 
 const inputArrayReturned = [];
 
-let numberArray = [];
 
 
 
-function Output(userInput) {
-  numberArray = createNumberArray(userInput);
-    for (let i = userInput; i >= 0; i--){ 
-    inputArrayReturned.push(i); 
-    inputArrayReturned.sort(); 
-      if(i.toString().includes(3)){
-        let newNumberArrayThrees = inputArrayReturned.replace("3", "Won't you be my neighbor?");
-      }else if (newNumberArrayThrees.includes(2)){
-        let newNumberArrayTwos = (newNumberArrayThrees.replace("2", "Boop!"));
-      }else if (newNumberArrayTwos.includes(1)){
-        let newNumberArrayOnes = (newNumberArrayTwos.replace("1", "Beep!"));
-      }else {
-        newNumberArrayOnes.push(i);
-      }
-    return newNumberArrayOnes;
-    };
+function Output(robogersInteger) {
+  for (let i = robogersInteger; i >= 0; i--){ 
+  inputArrayReturned.sort(); 
+    if(i.toString().includes(3)){
+      inputArrayReturned.replace("3", "Won't you be my neighbor?");
+    }else if (i.toString().includes(2)){
+      (inputArrayReturned.replace("2", "Boop!"));
+    }else if (i.toString.includes(1)){
+      newNumberArrayOnes.replace("1", "Beep!");
+    }else {
+      newNumberArrayOnes.push(i);
+    }
+  return newNumberArrayOnes;
+};
 
 
 
 
-function transformNumbers(inputArrayReturned) {
-  let numberArrayMinusThrees = transformNumbers.forEach(function(numberArray) {
-  if (inputArrayReturned.includes(3)){
-    newNumberArrayThrees = (inputArrayReturned.replace("3", "Won't you be my neighbor?"));
-  }
-  let numberArrayMinusTwos = transformNumbers.forEach(function(numberArray) {
-  else if (newNumberArrayThrees.includes(2)){
-    newNumberArrayTwos = (newNumberArrayTwos.replace("2", "Boop!"));
-  }
-  let numberArrayMinusOnes = transformNumbers.forEach(function(numberArray) {
-  else if (newNumberArrayTwos.includes(1)){
-    newNumberArrayOnes = (newNumberArrayTwos.replace("1", "Beep!"));
-  }
-  return transformNumbers(newNumberArrayOnes);
-});
+
+
+// function transformNumbers(inputArrayReturned) {
+//   let numberArrayMinusThrees = transformNumbers.forEach(function(numberArray) {
+//   if (inputArrayReturned.includes(3)){
+//     newNumberArrayThrees = (inputArrayReturned.replace("3", "Won't you be my neighbor?"));
+//   }
+//   let numberArrayMinusTwos = transformNumbers.forEach(function(numberArray) {
+//   else if (newNumberArrayThrees.includes(2)){
+//     newNumberArrayTwos = (newNumberArrayTwos.replace("2", "Boop!"));
+//   }
+//   let numberArrayMinusOnes = transformNumbers.forEach(function(numberArray) {
+//   else if (newNumberArrayTwos.includes(1)){
+//     newNumberArrayOnes = (newNumberArrayTwos.replace("1", "Beep!"));
+//   }
+//   return transformNumbers(newNumberArrayOnes);
+// });
 
 
 //UI Logic
 $(document).ready(function() {
   $("form#robogers-form").submit(function(event) {
     event.preventDefault(); 
-    const robogersResult = numberArrayMinusOnes(inputArrayReturned);
-    $("#result").text(robogersResult);
+    $("#result").text(newNumberArrayOnes);
     $("#result").show();
   };
 });
