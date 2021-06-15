b70bd437cf839be4bfd888cd85719411345e3851
Erika Debelis <Erika.Debelis@gmail.com>
2021-06-14 22:01:44 -0700

    BUGFIX: reorder html divs and button

--- js/scripts.js
+++ js/scripts.js
@@ -8,11 +8,11 @@ function Output(robogersInteger) {
   for (let i = 0; i <= robogersInteger; i++) { 
     const inputArrayReturned = [];
     if(i.toString().includes(3)){
-      inputArrayReturned.replace("3", "Won't you be my neighbor?");
+      inputArrayReturned.push("Won't you be my neighbor?");
     }else if (i.toString().includes(2)){
-      (inputArrayReturned.replace("2", "Boop!"));
+      (inputArrayReturned.push("2", "Boop!"));
     }else if (i.toString.includes(1)){
-      inputArrayReturned.replace("1", "Beep!");
+      inputArrayReturned.push("1", "Beep!");
     }else {
       inputArrayReturned.push(i);
     }
