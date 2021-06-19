974f1f46b947f1d515dad825d31606d3cf470ab0
Erika Debelis <Erika.Debelis@gmail.com>
2021-06-19 15:13:17 -0700

    BUGFIX:let userinput to pull value from input, not entire form

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,34 @@
 //Utlilty Logic
 
-let userInput = $("#robogersInteger").val(); 
 
 //Business Logic
 
-function Output(robogersInteger) {
-  for (let i = 0; i <= robogersInteger; i++) { 
-    const inputArrayReturned = [];
-    if(i.toString().includes(3)){
+function output(robogersInteger) {
+  const inputArrayReturned = [];
+  for (let i = 0; i <= robogersInteger; i++) {
+    if (i.toString().includes(3)) {
       inputArrayReturned.push("Won't you be my neighbor?");
-    }else if (i.toString().includes(2)){
-      (inputArrayReturned.push("2", "Boop!"));
-    }else if (i.toString.includes(1)){
-      inputArrayReturned.push("1", "Beep!");
-    }else {
+    } else if (i.toString().includes(2)) {
+      inputArrayReturned.push("Boop!");
+    } else if (i.toString().includes(1)) {
+      inputArrayReturned.push("Beep!");
+    } else {
       inputArrayReturned.push(i);
     }
+  }
   return inputArrayReturned;
 };
 
 //UI Logic
-$(document).ready(function() {
-  $("form#robogers-form").submit(function(event) {
-    event.preventDefault(); 
-    $("#result").text(inputArrayReturned);
+
+$(document).ready(function () {
+  $("#robogers-form").submit(function (event) {
+    event.preventDefault();
+    let userInput = $("#userInput").val();
+    let outputArrayReturned = output(userInput); 
+    let neighborCount = [];
+    neighborCount.push(outputArrayReturned.join(", "));
+    $("#result").text(neighborCount);
     $("#result").show();
-  }
-};
+  })
+});
